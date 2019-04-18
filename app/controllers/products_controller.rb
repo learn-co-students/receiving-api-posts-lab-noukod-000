a29a16149623b22b7dd8a72116976880ece2b6c2
Merline Belizaire <merline@noukod.com>
2019-04-18 00:50:54 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+
 class ProductsController < ApplicationController
   def index
     @products = Product.all
@@ -19,7 +20,7 @@ class ProductsController < ApplicationController
 
   def create
     Product.create(product_params)
-    redirect_to products_path
+     render json: @product, status: 201
   end
 
   def show
